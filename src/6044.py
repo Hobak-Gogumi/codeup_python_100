21e3eb77280ca9bf64f66b0a6119c6e58d624cf4
SEOHYEON LEE <seoooohs2@naver.com>
2022-02-24 18:27:18 +0900

    Add 6043, 6044, 6045

---
+++ src/6044.py
@@ -0,0 +1,17 @@
+n1, n2 = input().split()
+
+n3 = float(int(n1) / int(n2))
+print(int(n1) + int(n2))
+print(int(n1) - int(n2))
+print(int(n1) * int(n2))
+print(int(n1) // int(n2))
+print(int(n1) % int(n2))
+print(format(n3, ".2f"))
+
+"""
+답안은
+a = int(a)
+b = int(b)
+
+print(round(a/b, 2))
+"""
---
+++ src/6045.py
@@ -0,0 +1,10 @@
+n1, n2, n3 = input().split()
+
+n1 = int(n1)
+n2 = int(n2)
+n3 = int(n3)
+
+sum = n1 + n2 + n3
+avg = sum / 3
+
+print(sum, format(avg, ".2f"))
