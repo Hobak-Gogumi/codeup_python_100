3387571a2ef0572a1044c02c8692b6ba183450b0
SEOHYEON LEE <seoooohs2@naver.com>
2022-03-10 22:52:55 +0900

    Add 6087, 6088, 6089

---
+++ src/6088.py
@@ -0,0 +1,9 @@
+a, d, n = input().split()
+
+a = int(a)
+d = int(d)
+n = int(n)
+
+ans = a + ((n - 1) * d)
+
+print(ans)
---
+++ src/6089.py
@@ -0,0 +1,9 @@
+a, r, n = input().split()
+
+a = int(a)
+r = int(r)
+n = int(n)
+
+ans = a * (r ** (n-1))
+
+print(ans)
